182bafc353c6b76c13ea70fb16bc49c861bd87ee
yoszimitsu <cotam07@gmail.com>
2020-12-24 01:56:19 +0100

    CustomVisionMaskDetectionTags refactor/rebuild

--- src/main/java/objectDetection/CustomVisionMaskDetectionTags.java
+++ src/main/java/objectDetection/CustomVisionMaskDetectionTags.java
@@ -1,54 +1,50 @@
 package objectDetection;
 
+import dto.MaskDetectionRequestObject;
+import error.CredentialsNotFoundError;
 import javafx.scene.image.Image;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.opencv.core.*;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
-import java.io.*;
-import java.net.URI;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 import static openCv.ImageProcessing.mat2Img;
+import static services.CustomVisionService.jsonParser;
+import static services.CustomVisionService.sendRequest;
 
 public class CustomVisionMaskDetectionTags {
 
     private final double threshold = 0.5;
     private ArrayList<JSONObject> maskDetectionJSONObjectArray = new ArrayList<>();
-    private Rect[] rectArray;
     private String url;
     private String key;
 
-    public CustomVisionMaskDetectionTags() {
+    public CustomVisionMaskDetectionTags() throws CredentialsNotFoundError {
         getCredentials();
     }
 
-    public Image maskDetection(VideoCapture capture) throws IOException {
-        HttpClient httpclient = HttpClients.createDefault();
-        Mat mat = new Mat();
-        MatOfRect facesDetected = new MatOfRect();
-        ArrayList<HttpEntity> httpEntities = new ArrayList<>();
+    public Image maskDetection(VideoCapture capture) {
+        MaskDetectionRequestObject maskDetectionRequestObject = new MaskDetectionRequestObject();
+
         // GET IMAGE FROM VIDEO
-        capture.read(mat);
+        capture.read(maskDetectionRequestObject.getMat());
         // SEND IMAGE TO OPENCV FACE DETECTION
-        OpenCvFaceDetection openCvFaceDetection = new OpenCvFaceDetection(mat);
+        OpenCvFaceDetection openCvFaceDetection = new OpenCvFaceDetection(maskDetectionRequestObject.getMat());
         // GET FACE IMAGES DETECTED ON CAPTURE
         ArrayList<Mat> faceImageArray = openCvFaceDetection.getFaceImageArray();
-        rectArray = openCvFaceDetection.getRectArray();
+        Rect[] rectArray = openCvFaceDetection.getRectArray();
 
         for (Mat faceImage : faceImageArray) {
             try {
@@ -56,52 +52,22 @@ public class CustomVisionMaskDetectionTags {
                 Imgcodecs.imencode(".jpg", faceImage, bytes);
                 ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes.toArray());
 
-                URIBuilder builder = new URIBuilder(url);
-                URI uri = builder.build();
-                HttpPost request = new HttpPost(uri);
-                request.setHeader("Prediction-Key", key);
-                request.setHeader("Content-Type", "application/octet-stream");
+                // SEND HTTP REQUEST
+                HttpResponse response = sendRequest(maskDetectionRequestObject, inputStream, url, key);
 
-                // Request body
-                byte[] data = inputStream.readAllBytes();
-                HttpEntity requestEntity = new ByteArrayEntity(data);
-
-                request.setEntity(requestEntity);
-
-                // Send HTTP request
-                HttpResponse response = httpclient.execute(request);
-                HttpEntity entity = response.getEntity();
-
-                if (entity != null) {
-                    // ADD ENTITY TO ARRAY
-                    httpEntities.add(entity);
+                // CONVERT RESPONSE TO STRING AND CONVERT RESPONSE STRING TO JSON ARRAY
+                if (response.getEntity() != null) {
+                    maskDetectionRequestObject.getHttpEntities().add(response.getEntity());
                 }
+
+                maskDetectionJSONObjectArray = jsonParser(maskDetectionRequestObject.getHttpEntities(), maskDetectionJSONObjectArray, threshold);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        // PARSE JSON RESPONSES
-        jsonParser(httpEntities);
 
-        return mat2Img(rectangle(rectArray, mat, maskDetectionJSONObjectArray));
-    }
-
-    private void jsonParser(ArrayList<HttpEntity> httpEntitiesArray) throws IOException {
-        maskDetectionJSONObjectArray.clear();
-        for (HttpEntity entity : httpEntitiesArray) {
-            JSONObject jsonObjectResponse = new JSONObject(EntityUtils.toString(entity));
-            if (jsonObjectResponse.has("predictions")) {
-                JSONArray predictionsObjectArray = new JSONArray(jsonObjectResponse.get("predictions").toString());
-                for (int i = 0; i < predictionsObjectArray.length(); i++) {
-                    maskDetectionJSONObjectArray.add(new JSONObject(predictionsObjectArray.get(i).toString()));
-                }
-                // FILTER PROBABILITY > THRESHOLD
-                maskDetectionJSONObjectArray = (ArrayList<JSONObject>) maskDetectionJSONObjectArray
-                        .stream()
-                        .filter(jsonObject -> jsonObject.getDouble("probability") > threshold)
-                        .collect(Collectors.toList());
-            }
-        }
+        return mat2Img(rectangle(rectArray, maskDetectionRequestObject.getMat(), maskDetectionJSONObjectArray));
     }
 
     private Mat rectangle(Rect[] rectArray, Mat image, ArrayList<JSONObject> maskDetectionJSONObjectArray) {
@@ -112,7 +78,7 @@ public class CustomVisionMaskDetectionTags {
                     Rect face = rectArray[i];
                     if (maskDetectionJSONObjectArray.get(i).getString("tagName").equals("mask")) {
                         Imgproc.rectangle(image, face.tl(), face.br(), new Scalar(0, 255, 0), 3);
-                        Imgproc.putText(image, "mask pr:" + probability , face.tl(), 1, 1, new Scalar(0, 255, 0), 1);
+                        Imgproc.putText(image, "mask pr:" + probability, face.tl(), 1, 1, new Scalar(0, 255, 0), 1);
                     }
                     if (maskDetectionJSONObjectArray.get(i).getString("tagName").equals("noMask")) {
                         Imgproc.rectangle(image, face.tl(), face.br(), new Scalar(0, 0, 255), 3);
@@ -126,21 +92,17 @@ public class CustomVisionMaskDetectionTags {
         return image;
     }
 
-    private void getCredentials() {
+    private void getCredentials() throws CredentialsNotFoundError {
         Properties prop = new Properties();
-        String fileName = "./src/main/resources/app.config";
-        InputStream is = null;
+        String fileName = "./src/main/java/credentials/app.config";
         try {
-            is = new FileInputStream(fileName);
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        try {
+            InputStream is = new FileInputStream(fileName);
             prop.load(is);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            this.url = prop.getProperty("url.customVisionTags");
+            this.key = prop.getProperty("key.customVisionTags");
+        } catch (Exception e) {
+            throw new CredentialsNotFoundError();
         }
-        this.url = prop.getProperty("url.customVisionTags");
-        this.key = prop.getProperty("key.customVisionTags");
     }
+
 }
