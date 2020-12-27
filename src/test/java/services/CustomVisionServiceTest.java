3f4176380df0c242dd65d334ca8d7d8aa1fe53b9
yoszimitsu <cotam07@gmail.com>
2020-12-27 21:53:08 +0100

    added @BeforeClass annotation in CustomVisionServiceTest

--- src/test/java/services/CustomVisionServiceTest.java
+++ src/test/java/services/CustomVisionServiceTest.java
@@ -1,12 +1,22 @@
+package services;
+
 import dto.AzurePassDto;
+import dto.MaskDetectionRequestDto;
 import error.CredentialsFileNotFound;
 import error.CredentialsNotFoundError;
 import error.JsonResponseParserError;
+import nu.pattern.OpenCV;
+import openCv.ImageProcessing;
+import org.apache.http.HttpResponse;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import services.CustomVisionService;
+import org.opencv.core.Core;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -14,11 +24,15 @@ import java.util.Scanner;
 
 public class CustomVisionServiceTest {
 
+    @BeforeClass
+    public static void setUp() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        OpenCV.loadShared();
+    }
+
     @Test
     public void getCredentialsShouldReturnTestCredentials() throws CredentialsNotFoundError, CredentialsFileNotFound {
-        AzurePassDto azurePassDto;
-
-        azurePassDto = CustomVisionService.getCredentials("./src/test/java/resources/testApp.config", "url.test", "key.test");
+        AzurePassDto azurePassDto = CustomVisionService.getCredentials("./src/test/java/resources/test.config", "url.test", "key.test");
 
         Assert.assertEquals("url", azurePassDto.getUrl());
         Assert.assertEquals("key", azurePassDto.getKey());
@@ -28,7 +42,7 @@ public class CustomVisionServiceTest {
     public void getCredentialsShouldThrowCredentialNotFoundError() {
         CredentialsNotFoundError exception = Assert.assertThrows(
                 CredentialsNotFoundError.class,
-                () -> CustomVisionService.getCredentials("./src/test/java/resources/testApp.config", "url.testNotFound", "key.testNotFound"));
+                () -> CustomVisionService.getCredentials("./src/test/java/resources/test.config", "url.testNotFound", "key.testNotFound"));
 
         Assert.assertEquals("Credentials not found. Check variable names.", exception.getMessage());
     }
@@ -47,7 +61,7 @@ public class CustomVisionServiceTest {
         ArrayList<JSONObject> jsonObjectArrayList = new ArrayList<>();
         double threshold = 0.5;
 
-        JSONObject jsonObject = readJsonFromFile("./src/test/java/resources/response.txt");
+        JSONObject jsonObject = readJsonFromFile("./src/test/java/resources/json/response.txt");
         jsonObjectArrayList = CustomVisionService.jsonParser(jsonObject, jsonObjectArrayList, threshold);
 
         Assert.assertEquals(1, jsonObjectArrayList.size());
@@ -59,7 +73,7 @@ public class CustomVisionServiceTest {
         ArrayList<JSONObject> jsonObjectArrayList = new ArrayList<>();
         double threshold = 1;
 
-        JSONObject jsonObject = readJsonFromFile("./src/test/java/resources/response.txt");
+        JSONObject jsonObject = readJsonFromFile("./src/test/java/resources/json/response.txt");
         jsonObjectArrayList = CustomVisionService.jsonParser(jsonObject, jsonObjectArrayList, threshold);
 
         Assert.assertEquals(0, jsonObjectArrayList.size());
@@ -69,7 +83,7 @@ public class CustomVisionServiceTest {
     public void jsonParserReturnJsonResponseParserError() {
         ArrayList<JSONObject> jsonObjectArrayList = new ArrayList<>();
         double threshold = 0.5;
-        JSONObject jsonObject = readJsonFromFile("./src/test/java/resources/badResponse.txt");
+        JSONObject jsonObject = readJsonFromFile("./src/test/java/resources/json/badResponse.txt");
 
         ArrayList<JSONObject> finalJsonObjectArrayList = jsonObjectArrayList;
         JsonResponseParserError exception = Assert.assertThrows(
@@ -77,6 +91,37 @@ public class CustomVisionServiceTest {
                 () -> CustomVisionService.jsonParser(jsonObject, finalJsonObjectArrayList, threshold));
 
         Assert.assertEquals("Error during parsing JSON response.", exception.getMessage());
+    }
+
+    @Test
+    public void sendRequestShouldReturn200StatusCode() throws CredentialsNotFoundError, CredentialsFileNotFound {
+        MaskDetectionRequestDto maskDetectionRequestDto = new MaskDetectionRequestDto();
+        maskDetectionRequestDto.setMat(ImageProcessing.loadImage("./src/test/java/resources/images/testMaskImg.jpg"));
+        MatOfByte bytes = new MatOfByte();
+        Imgcodecs.imencode(".jpg", maskDetectionRequestDto.getMat(), bytes);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes.toArray());
+
+        //Before test execution, be sure that in test.config file are correct Azure url and key (url.customVision, key.customVision)
+        AzurePassDto azurePassDto = CustomVisionService.getCredentials("./src/test/java/resources/test.config", "url.customVision", "key.customVision");
+        HttpResponse response = CustomVisionService.sendRequest(maskDetectionRequestDto, inputStream, azurePassDto.getUrl(), azurePassDto.getKey());
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void sendRequestShouldReturn404StatusCode() throws CredentialsNotFoundError, CredentialsFileNotFound {
+        MaskDetectionRequestDto maskDetectionRequestDto = new MaskDetectionRequestDto();
+        maskDetectionRequestDto.setMat(ImageProcessing.loadImage("./src/test/java/resources/images/testMaskImg.jpg"));
+        MatOfByte bytes = new MatOfByte();
+        Imgcodecs.imencode(".jpg", maskDetectionRequestDto.getMat(), bytes);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes.toArray());
+
+        AzurePassDto azurePassDto = CustomVisionService.getCredentials("./src/test/java/resources/test.config", "url.customVisionWrong", "key.customVisionWrong");
+        HttpResponse response = CustomVisionService.sendRequest(maskDetectionRequestDto, inputStream, azurePassDto.getUrl(), azurePassDto.getKey());
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(404, response.getStatusLine().getStatusCode());
     }
 
     private JSONObject readJsonFromFile(String filePath) {
