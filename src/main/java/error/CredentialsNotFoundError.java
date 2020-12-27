c0d2ec6cd2a5960155848cdefca8e38bd0053060
yoszimitsu <cotam07@gmail.com>
2020-12-27 16:59:36 +0100

    add CredentialsFileNotFound && CredentialsNotFoundError refactor

--- src/main/java/error/CredentialsNotFoundError.java
+++ src/main/java/error/CredentialsNotFoundError.java
@@ -2,7 +2,7 @@ package error;
 
 public class CredentialsNotFoundError extends Exception {
 
-    public CredentialsNotFoundError() {
-        super("Credentials not found. Check if app.config file exists.");
+    public CredentialsNotFoundError(String message) {
+        super(message);
     }
 }
